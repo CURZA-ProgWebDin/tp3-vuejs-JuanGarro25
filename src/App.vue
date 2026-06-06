d23b5f9cb45f855b6629c0adc646b07ca705b39e
Juan Garro <comahue2025jg@gmail.com>
2026-06-05 22:26:14 -0300

    Add files via upload
    TP3 - Catálogo de Productos

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+
+<template>
+  <main class="contenedor">
+    <h1 class="titulo-principal"> Catálogo de Productos </h1>
+
+    <PanelPestanas />
+  </main>
+</template>
+
+<script setup>
+import PanelPestanas from './PanelPestanas.vue'
+</script>
---
+++ src/components/tabs/TabElectronica.vue
@@ -0,0 +1,22 @@
+
+<template>
+  <ListaProductos :productos="productosElectronica" />
+</template>
+
+<script setup>
+import {computed, onMounted, onUnmounted, onActivated, onDeactivated} from 'vue'
+import { productos } from '../../data/productos'
+import ListaProductos from '../ListaProductos.vue'
+
+const productosElectronica = computed(() =>
+  productos.filter(
+    p => p.categoria === 'Electrónica'
+  )
+)
+
+onMounted(() => console.log('TabElectronica - montado'))
+onUnmounted(() => console.log('TabElectronica - desmontado'))
+onActivated(() => console.log('TabElectronica - activado'))
+onDeactivated(() => console.log('TabElectronica - desactivado'))
+</script>
+
